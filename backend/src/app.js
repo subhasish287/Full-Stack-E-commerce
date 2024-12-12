a185c54219d4c54daf7261805c6ae435958204a5
Santu506 <140848902+Santu506@users.noreply.github.com>
2024-12-12 19:48:02 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,12 @@ import productRouter from './routes/product.routes.js';
 import cartRouter from './routes/cart.routes.js';
 import orderRouter from './routes/order.routes.js';
 import cors from 'cors';
-app.use(cors());
+app.use(cors({
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        origin: ['https://full-stack-e-commerce-k455.vercel.app'],  // Or specify the origin like 'http://localhost:5173'
+        credentials: true  // Allow cookies or credentials to be sent
+            
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
