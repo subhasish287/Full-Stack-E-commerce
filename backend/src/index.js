2c5f90138eb09034320f0804590b60c27ac3cdee
Santu506 <140848902+Santu506@users.noreply.github.com>
2024-12-12 19:41:18 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 dotenv.config();
 app.use(cors({
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        origin: ['https://full-stack-e-commerce-frontend-iota.vercel.app'],  // Or specify the origin like 'http://localhost:5173'
+        origin: ['https://full-stack-e-commerce-k455.vercel.app'],  // Or specify the origin like 'http://localhost:5173'
         credentials: true  // Allow cookies or credentials to be sent
             
 }));
