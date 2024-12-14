ce88378a04196f45e7ffb31e9f532a246869aa83
Santu ROy <santuroy72126060@gmail.com>
2024-12-14 23:08:46 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,24 +12,25 @@ dotenv.config();
 //         origin: ['https://full-stack-e-commerce-frontend-iota.vercel.app'],  // Or specify the origin like 'http://localhost:5173'
 //         credentials: true  // Allow cookies or credentials to be sent            
 // }));
-connectDb();
-connectCloudinary();
-app.get('/', (req, res) => {
-        res.send("APi Working");
-})
-app.get("/test", (req, res) => {
-        res.send("Test Working");
-})
+// connectDb();
+// connectCloudinary();
+// app.get('/', (req, res) => {
+//         res.send("APi Working");
+// })
+// app.get("/test", (req, res) => {
+//         res.send("Test Working");
+// })
 
-app.listen(process.env.PORT, () => {
-        console.log("Server is running on port 8080");
-})
-app.use(cors({
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        origin: ['https://full-stack-e-commerce-k455.vercel.app'],  // Or specify the origin like 'http://localhost:5173'
-        credentials: true  // Allow cookies or credentials to be sent
+// app.listen(process.env.PORT, () => {
+//         console.log("Server is running on port 8080");
+// })
+// app.use(cors({
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         origin: ['https://full-stack-e-commerce-k455.vercel.app'],  // Or specify the origin like 'http://localhost:5173'
+//         credentials: true  // Allow cookies or credentials to be sent
             
-}));
+// }));
+
 connectDb();
 connectCloudinary();
 
