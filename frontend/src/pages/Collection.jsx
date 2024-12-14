19eda8d2d3cdf4593a45342d553a82744fa9c121
Santu ROy <santuroy72126060@gmail.com>
2024-12-15 03:46:35 +0530

    ./lkjh

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -89,13 +89,16 @@ function Collection() {
             <p className='mb-3 text-sm font-medium'>Categorories</p>
             <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
               <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Men'} onChange={toggleCategory}/>Men
+                  <input className='w-3' id='Men' type="checkbox" value={'Men'} onChange={toggleCategory}/>
+                  <label htmlFor="Men" className='cursor-pointer'>Men</label>
               </p>
               <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory}/>Women
+                  <input className='w-3' type="checkbox" id='Women' value={'Women'} onChange={toggleCategory}/>
+                  <label htmlFor="Women" className='cursor-pointer'>Women</label>
               </p>
               <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory}/>Kids
+                  <input className='w-3' type="checkbox" id='Kids' value={'Kids'} onChange={toggleCategory}/>
+                  <label htmlFor="Kids" className='cursor-pointer'>Kids</label>
               </p>
             </div>
         </div>
@@ -104,13 +107,16 @@ function Collection() {
             <p className='mb-3 text-sm font-medium'>Type</p>
             <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
               <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/>Topwear
+                  <input className='w-3' type="checkbox" id='Topwear' value={'Topwear'} onChange={toggleSubCategory}/>
+                  <label htmlFor="Topwear" className='cursor-pointer'>Topwear</label>
               </p>
               <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory}/>Bottomwear
+                  <input className='w-3' type="checkbox" id='Bottomwear' value={'Bottomwear'} onChange={toggleSubCategory}/>
+                  <label htmlFor="Bottomwear" className='cursor-pointer'>Bottomwear</label>
               </p>
               <p className='flex gap-2'>
-                  <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/>Winterwear
+                  <input className='w-3' type="checkbox" id='Winterwear' value={'Winterwear'} onChange={toggleSubCategory}/>
+                  <label htmlFor="Winterwear" className='cursor-pointer'>Winterwear</label>
               </p>
             </div>
         </div>
@@ -121,10 +127,10 @@ function Collection() {
           <div className='flex justify-between text-base sm:text-2xl mb-4'>
               <Title text1={'All'} text2={'Collections'}/>
               {/* Product Sort */}
-              <select onChange={(e)=>setSortTYpe(e.target.value)} className='border-2 border-gray-300 text-sm px-2' name="" id="">
-                <option value="relavent">Sort by:Relavent</option>
-                <option value="low-high">Sort by:Low to High</option>
-                <option value="high-low">Sort by:High to Low</option>
+              <select onChange={(e)=>setSortTYpe(e.target.value)} className='border-2 border-gray-300 text-sm px-2 cursor-pointer' name="" id="" >
+                <option value="relavent" className='cursor-pointer' >Sort by:Relavent</option>
+                <option value="low-high" className='cursor-pointer'>Sort by:Low to High</option>
+                <option value="high-low" className='cursor-pointer'>Sort by:High to Low</option>
               </select>
           </div>
 
