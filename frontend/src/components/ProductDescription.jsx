2ee727090a3d7fa4f41fe2a0be1cfa466a86ad6e
Santu ROy <santuroy72126060@gmail.com>
2024-12-15 03:51:00 +0530

    qws

--- frontend/src/components/ProductDescription.jsx
+++ frontend/src/components/ProductDescription.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const ProductDescription = () => {
   return (
     <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
-          <p className="mb-3">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui atque
-            repellat ea a omnis sint ex iusto possimus delectus dolorum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
-            inventore maxime optio nemo illum, mollitia rerum! Dicta velit aut
-            quas magni numquam. Iste nam molestiae saepe delectus dolore fugiat
-            amet.
-          </p>
-        </div>
-  )
-}
+      <p className="mb-3">
+        A dress is a one-piece garment that covers the body from the shoulders
+        to varying lengths, typically worn by women or girls. Dresses come in a
+        wide range of styles, fabrics, and designs, making them versatile for
+        occasions ranging from casual wear to formal events. They can feature
+        various necklines, sleeves, and embellishments, tailored to suit
+        personal preferences or cultural norms. Dresses are a timeless and
+        elegant clothing choice, often celebrated for their comfort and
+        aesthetic appeal.
+      </p>
+      
+    </div>
+  );
+};
 
-export default ProductDescription
+export default ProductDescription;
