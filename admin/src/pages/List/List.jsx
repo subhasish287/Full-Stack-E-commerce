fa854088456d295ce9269bdff79badfb8e2efe04
Santu ROy <santuroy72126060@gmail.com>
2024-12-16 23:14:51 +0530

    some problem

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./List.css";
 import axios from "axios";
 import { toast } from "react-toastify";
-import Update from "../Update/update";
+import Update from "../Update/Update";
 
 function List({ url, token }) {
   const [list, setList] = useState([]);
